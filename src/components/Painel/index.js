70db2448e36c9fa43f475a80d7a090c3248fd71b
unknown <rafaelbatista.sth@gmail.com>
2020-06-07 22:39:42 -0300

    logout firebase

--- src/components/Painel/index.js
+++ src/components/Painel/index.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useContext, useEffect, Fragment } from "react";
+import { FirebaseContext } from "../Firebase";
 import Logout from "../Logout";
 import Quiz from "../Quiz";
 
-const Welcome = () => {
-  return (
+const Welcome = (props) => {
+  const firebase = useContext(FirebaseContext);
+
+  const [userSession, setUserSession] = useState(null);
+
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged((user) => {
+      user ? setUserSession(user) : props.history.push("/");
+    });
+
+    return () => {
+      listener();
+    };
+  }, []);
+
+  return userSession === null ? (
+    <Fragment>
+      <div className="loader">
+        <p>loading...</p>
+      </div>
+    </Fragment>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
