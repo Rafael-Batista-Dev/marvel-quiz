74691fae8c08bd19432a748d93ba117993f4b84c
unknown <rafaelbatista.sth@gmail.com>
2020-06-08 18:11:53 -0300

    api quiz

---
+++ src/components/quizMarvel/index.js
@@ -0,0 +1,300 @@
+export const QuizMarvel = [
+  {
+    providerQuiz:
+      "Rafael Batista (https://github.com/Rafael-Batista-Dev/marvel-quiz)",
+    topic:
+      "Aplicação de Quiz, com perguntas da Marvel desenvolvida com React JS",
+    quizz: {
+      beginner: [
+        {
+          id: 0,
+          question: "A qual time o Iceman pertence?",
+          options: [
+            "Os X-Men",
+            "Os quatro fantásticos",
+            "Os Invasores",
+            "Os Libertadoras",
+          ],
+          answer: "Os X-Men",
+          heroId: 1009362,
+        },
+        {
+          id: 1,
+          question:
+            "Nos X-Men, que substância constitui o esqueleto de Wolverine?",
+          options: ["Aço Inox", "Vibranium", "Adamantium", "Neutrônio"],
+          answer: "Adamantium",
+          heroId: 1009718,
+        },
+        {
+          id: 2,
+          question:
+            "Qual é o nome do Homem-Aranha, que apareceu pela primeira vez em 1962?",
+          options: ["Tom Parker", "John Parker", "Peter Parker", "Alan Parker"],
+          answer: "Peter Parker",
+          heroId: 1009610,
+        },
+        {
+          id: 3,
+          question:
+            "Qual era o nome de Wolverine quando ele era um cavaleiro do Apocalipse?",
+          options: ["Raiva", "Morte", "Destruição", "Esquecimento"],
+          answer: "Morte",
+          heroId: 1009718,
+        },
+        {
+          id: 4,
+          question:
+            "Que super-herói nunca se separou de seu martelo forjado por anões?",
+          options: ["Bizarro", "Flash", "Venom", "Thor"],
+          answer: "Thor",
+          heroId: 1009664,
+        },
+        {
+          id: 5,
+          question: "Qual cor está associada ao Hulk?",
+          options: ["O Amarelo", "O vermelho", "O verde", "O Azul"],
+          answer: "O verde",
+          heroId: 1009351,
+        },
+        {
+          id: 6,
+          question: "Que super-herói se parece com um ser de pedra?",
+          options: [
+            "Homem de plástico",
+            "O coisa",
+            "Homem falcão",
+            "Homem pedra",
+          ],
+          answer: "O coisa",
+          heroId: 1009662,
+        },
+        {
+          id: 7,
+          question: "Que super-herói possui uma armadura tecnologica?",
+          options: [
+            "Bruce Wayne",
+            "Charles Xavier",
+            "Tony Stark",
+            "Henry Steven",
+          ],
+          answer: "Tony Stark",
+          heroId: 1009368,
+        },
+        {
+          id: 8,
+          question:
+            "Qual super-herói usa uma fantasia inspirada na bandeira americana?",
+          options: [
+            "O Pantera Negra",
+            "Lâmina",
+            "Homem de Ferro",
+            "Capitão América",
+          ],
+          answer: "Capitão América",
+          heroId: 1009220,
+        },
+        {
+          id: 9,
+          question:
+            "Incluindo os polegares, quantos dedos nas duas mãos tem O coisa?",
+          options: [8, 6, 10, 4],
+          answer: 8,
+          heroId: 1009662,
+        },
+      ],
+      confirme: [
+        {
+          id: 0,
+          question: "Quem é Logan nos quadrinhos da Marvel Comics 'X-Men'?",
+          options: ["Cyclope", "Colossus", "Wolverine", "Caliban"],
+          answer: "Wolverine",
+          heroId: 1009718,
+        },
+        {
+          id: 1,
+          question: "Qual super-herói está interessado na tempestade",
+          options: ["Charles Xavier", "Thor", "Homem aranha", "Namor"],
+          answer: "Namor",
+          heroId: 1009466,
+        },
+        {
+          id: 2,
+          question:
+            "Que humano se torna forte utilizando uma armaduras de alta tecnologia?",
+          options: ["Cyclope", "Homem de ferro", "Wolverine", "Thor"],
+          answer: "Homem de ferro",
+          heroId: 1009368,
+        },
+        {
+          id: 3,
+          question: "Qual desses é inimigo emblemático do Homem-Aranha?",
+          options: ["Lagarto", "Thanos", "Cobra", "Green Goblin"],
+          answer: "Green Goblin",
+          heroId: 1014985,
+        },
+        {
+          id: 4,
+          question: "Motoqueiro Fantasma é conhecido como: ",
+          options: [
+            "O diabo motociclista",
+            "O espírito de ódio",
+            "O espírito da vingança",
+            "O crânio vermelho",
+          ],
+          answer: "O espírito da vingança",
+          heroId: 1009318,
+        },
+        {
+          id: 5,
+          question: "Antes de se tornar um Homem Radioativo, Chen Lu foi ...",
+          options: [
+            "Um espião",
+            "Um físico nuclear",
+            "Um soldado",
+            "Um piloto",
+          ],
+          answer: "Um físico nuclear",
+          heroId: 1009526,
+        },
+        {
+          id: 6,
+          question:
+            "Qual super-herói que perdeu a visão desenvolveu órgãos sensoriais?",
+          options: ["Demolidor", "Flash", "Superman", "Capitão América"],
+          answer: "Demolidor",
+          heroId: 1009262,
+        },
+        {
+          id: 7,
+          question: "Antes de entrar para avida de crime, Mysterio era ...",
+          options: [
+            "Dublê",
+            "Artista de efeitos especiais",
+            "Mago",
+            "Bombeiro",
+          ],
+          answer: "Artista de efeitos especiais",
+          heroId: 1010952,
+        },
+        {
+          id: 8,
+          question: "Fin Fang Foom é um alienígena ...",
+          options: ["Krypton", "Maklu IV", "Marte", "Vulcan"],
+          answer: "Maklu IV",
+          heroId: 1009303,
+        },
+        {
+          id: 9,
+          question: "DOUTOR DOOM frequentou a mesma faculdade que ...",
+          options: [
+            "Tony Stark",
+            "Peter Parker",
+            "Reed Richards",
+            "Bruce Banner",
+          ],
+          answer: "Reed Richards",
+          heroId: 1009281,
+        },
+      ],
+      expert: [
+        {
+          id: 0,
+          question:
+            "Quando ela trabalhou para Hydra, a primeira missão da Mulher-Aranha era ...",
+          options: [
+            "Seduzir o Homem de Ferro",
+            "Seduzir o Homem aranha",
+            "Matar a viúva negra ",
+            "Matar Nick Fury",
+          ],
+          answer: "Matar Nick Fury",
+          heroId: 1009608,
+        },
+        {
+          id: 1,
+          question:
+            "Qual super-herói criado pela Marvel Comics é cego desde o nascimento?",
+          options: ["Flash", "Thor", "Cyclope", "Stick"],
+          answer: "Stick",
+          heroId: 1009626,
+        },
+        {
+          id: 2,
+          question:
+            "Spider-Man foi convidado a criar o Spider-Mobile pela empresa de publicidade da ...",
+          options: [
+            "Carter Lombardo",
+            "Max Shrek",
+            "Reed Richards",
+            "Spider Company",
+          ],
+          answer: "Carter Lombardo",
+          heroId: 1009610,
+        },
+        {
+          id: 3,
+          question: "Qual heroína do Demolidor é uma assassina profissional?",
+          options: ["Ultron", "Mulher Gato", "Elektra", "Mulher maravilha"],
+          answer: "Elektra",
+          heroId: 1009288,
+        },
+        {
+          id: 4,
+          question:
+            "Onde reside o mandarim, que recentemente se tornou patrono de Sin-Cong Wong Chu?",
+          options: [
+            "O Vale dos Espíritos",
+            "No Juazeiro do norte",
+            "Os cânions da Atlântida",
+            "A Caverna das Almas",
+          ],
+          answer: "O Vale dos Espíritos",
+          heroId: 1009421,
+        },
+        {
+          id: 5,
+          question:
+            "Qual super-herói tem os mesmos poderes que o Homem-Aranha?",
+          options: ["Venom", "Arqueiro verde", "Lâmina", "O coisa"],
+          answer: "Venom",
+          heroId: 1010788,
+        },
+        {
+          id: 6,
+          question:
+            "Nick Fury tem um irmão que se tornou mau. Qual é o nome dele?",
+          options: ["Venom", "Víbora", "O crânio vermelho", "Escorpião"],
+          answer: "Escorpião",
+          heroId: 1011007,
+        },
+        {
+          id: 7,
+          question: "Miss Marvel nunca foi conhecida como:",
+          options: ["Capitão Marvel", "Vampira", "Warbird", "Meninas Poderosa"],
+          answer: "Vampira",
+          heroId: 1010338,
+        },
+        {
+          id: 8,
+          question: "Quem é a heroína próxima de Dotor Stranho criada em 1964?",
+          options: ["Felina", "Clea", "Monica", "Elsa"],
+          answer: "Clea",
+          heroId: 1009239,
+        },
+        {
+          id: 9,
+          question: "Quando criança, Tempestade tentou ...",
+          options: [
+            "Queime uma vila com um raio",
+            "Torne-se um líder de gangue",
+            "Roubar o carro do professor Xavier",
+            "Vazar os bolsos do professor Xavier",
+          ],
+          answer: "Vazar os bolsos do professor Xavier",
+          heroId: 1009629,
+        },
+      ],
+    },
+  },
+];
