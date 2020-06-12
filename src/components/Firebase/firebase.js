2786634692dcdb155356f5243dde3779e1646b02
unknown <rafaelbatista.sth@gmail.com>
2020-06-12 20:32:52 -0300

    firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -6,13 +6,13 @@ import "firebase/firestore";
 
 const config = {
   //Suas credenciais
-  apiKey: "AIzaSyCthuPazeILrkOxvAFpukiprYYGvaxvhGQ",
-  authDomain: "marvel-quiz-5a137.firebaseapp.com",
-  databaseURL: "https://marvel-quiz-5a137.firebaseio.com",
-  projectId: "marvel-quiz-5a137",
-  storageBucket: "marvel-quiz-5a137.appspot.com",
-  messagingSenderId: "802611792647",
-  appId: "1:802611792647:web:71a74617865a166ee613a2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 class Firebase {
   constructor() {
